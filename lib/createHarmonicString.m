3b216a03f0ca458951a27ba88ad3672d3628b926
David Pescariu <davidpescariu12@gmail.com>
2024-12-31 22:03:47 +0200

    feat: createHarmonicString

---
+++ lib/createHarmonicString.m
@@ -0,0 +1,25 @@
+%% createHarmonicString
+% Create a string to represent a harmonic: A * (2 * π * f * t) + ϕ
+% 
+% Inputs:
+%   A   - Amplitude, >= 1, numeric
+%   f   - Frequency, >= 1, numeric
+%   t   - Time value, numeric
+%   phi - Phase, numeric
+% 
+% Outputs:
+%   s   - Harmonic signal, string
+function s = createHarmonicString(A, f, t, phi)
+    arguments
+        A (1, :) {mustBeNumeric}
+        f (1, :) {mustBeNumeric}
+        t {mustBeNumeric}
+        phi {mustBeNumeric}
+    end
+
+    if phi ~= 0
+        s = A + " * " + "(2 * π * " + f + " * t) + " + phi;
+    else
+        s = A + " * " + "(2 * π * " + f + " * t)";
+    end
+end
