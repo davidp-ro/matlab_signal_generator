0a6f652d49b32fb37a62c62b74174bcac9e14060
David Pescariu <davidpescariu12@gmail.com>
2025-01-01 14:09:15 +0200

    chore: docs

--- main.m
+++ main.m
@@ -1,3 +1,33 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                             Signal Generator                            %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% Visualize harmonic signals and how they can be used to create a resulting
+% harmonic - essentially generate any waveform, composed of a sum of simple
+% "x(t) = A * cos(2 * π * f * t) + ϕ" equations.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% Folder Structure:
+%   > main.m - this file
+%   > pages - contains the layouts & page logic
+%       -> homePage.m - home page, view docs & open generator
+%       -> generatorPage.m - generator page, create the signals & view plot
+%   > lib - misc. pieces of logic & resources
+%       -> openGenerator.m / openDocs.m - self explanatory
+%       -> createHarmonic - takes in the params of the eq., and builds it
+%       -> createHarmonicString - builds the 'pretty' repr. of the eq.
+%       -> createHarmonicFromString - reconstructs the value of the eq.,
+%                           starting from the 'pretty' representation.
+% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% Repo Link: https://github.com/davidp-ro/matlab_signal_generator
+% 
+% CAG 2024-2025, David Pescariu, UTCN/ETTI
+% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 clc;
 close all;
 
